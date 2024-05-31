36bbecfee8848daf623cb9d351639e89a19fc015
Ran <ranglikman@gmail.com>
2024-05-31 15:42:41 +0300

    Refactor: Using note.service.js

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,35 @@
-// note service
+export const initialNotes = [
+    {
+        id: 'n101',
+        createdAt: 1112222,
+        type: 'NoteTxt',
+        isPinned: true,
+        style: { backgroundColor: '#CCFF90' },
+        info: {
+            txt: 'Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! ',
+        },
+    },
+    {
+        id: 'n102',
+        type: 'NoteImg',
+        isPinned: false,
+        info: {
+            url: 'https://www.ynet.co.il/PicServer2/24012010/2522593/bobi_gd.jpg',
+            title: 'Bobi and Me',
+        },
+        style: { backgroundColor: '#F28B82' },
+    },
+    {
+        id: 'n103',
+        type: 'NoteTodos',
+        isPinned: false,
+        info: {
+            title: 'Get my stuff together',
+            todos: [
+                { txt: 'Driving license', doneAt: null },
+                { txt: 'Keeping at it!', doneAt: null },
+                { txt: 'Coding power', doneAt: 187111111 },
+            ],
+        },
+    },
+]
--- apps/note/views/NoteIndex.jsx
+++ apps/note/views/NoteIndex.jsx
@@ -1,38 +1,4 @@
-const initialNotes = [
-    {
-        id: 'n101',
-        createdAt: 1112222,
-        type: 'NoteTxt',
-        isPinned: true,
-        style: { backgroundColor: '#CCFF90' },
-        info: {
-            txt: 'Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! Fullstack Me Baby! ',
-        },
-    },
-    {
-        id: 'n102',
-        type: 'NoteImg',
-        isPinned: false,
-        info: {
-            url: 'https://www.ynet.co.il/PicServer2/24012010/2522593/bobi_gd.jpg',
-            title: 'Bobi and Me',
-        },
-        style: { backgroundColor: '#F28B82' },
-    },
-    {
-        id: 'n103',
-        type: 'NoteTodos',
-        isPinned: false,
-        info: {
-            title: 'Get my stuff together',
-            todos: [
-                { txt: 'Driving license', doneAt: null },
-                { txt: 'Keeping at it!', doneAt: null },
-                { txt: 'Coding power', doneAt: 187111111 },
-            ],
-        },
-    },
-]
+import { initialNotes } from '../services/note.service.js'
 
 const NoteActions = ({ noteId, onColorChange, onCopy, onTogglePin, onDelete, isPinned }) => (
     <div className='note-actions'>
